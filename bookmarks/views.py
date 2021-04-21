b2211563fcc15c47de36663fdd847269a7ea243b
HP <logeswari5>
2021-04-21 16:53:25 +0530

    Add context in Bookmark detail view to get tags

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,21 +1,23 @@
-
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import bookmarks
-
 
 def index(request):
     return render(request, 'index.html')
 
 class BookmarkDetailView(generic.DetailView):
     model = bookmarks
-    paginate_by = 2
+    paginate_by = 10
 
+    def get_context_data(self, **kwargs):
+        context = super(BookmarkDetailView, self).get_context_data(**kwargs)
+        context['tags'] = [], self.object.tag.all()
+        return context
 
 class BookmarkListView(generic.ListView):
     model = bookmarks
-    paginate_by = 2
+    paginate_by = 10
 
 
 class CreateBookmark(generic.CreateView):
@@ -33,3 +35,4 @@ class DeleteBookmark(generic.DeleteView):
     model = bookmarks
     template_name = 'bookmarks/bookmarks_delete.html'
     success_url = reverse_lazy('bookmarks')
+
